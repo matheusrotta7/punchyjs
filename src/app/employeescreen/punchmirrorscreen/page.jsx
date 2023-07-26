7ede67c1c243028585a608fddf9dcf89fa7dd146
Matheus Rotta <matheusrotta7@gmail.com>
2023-07-25 21:24:24 -0300

    add sidebar elements of date and hour

--- src/app/employeescreen/punchmirrorscreen/page.jsx
+++ src/app/employeescreen/punchmirrorscreen/page.jsx
@@ -84,12 +84,45 @@ export default function punchScreen() {
             <aside className="float-right bg-zinc-950 flex-1 border-l border-zinc-800">
                 <div className="flex text-center justify-center flex-col">
                     <h1>sidebar to the right</h1>
-                    <div className="flex text-center justify-center mt-20">
+                    <div className="flex text-center justify-center mt-20 text-xl">
                         <MonthYearHeader 
                                 month={curMonth}
                                 year={curYear}
                         />
-                        
+                    </div>
+                    <div>
+                        <div className="relative bg-zinc-800 px-10 mt-5 pt-10 pb-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto  sm:rounded-full w-40 h-40">
+                            <div className="flex flex-col">
+                                <div className="text-xl">
+                                    Thursday    
+                                </div>
+                                <div className="text-5xl mt-2">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row mt-4 justify-center">
+                        <div className="relative bg-zinc-800  py-3 shadow-xl ring-1 ring-gray-900/5   sm:rounded-full w-12 h-12 mx-2">09:34</div>
+                        <div className="relative bg-zinc-800  py-3 shadow-xl ring-1 ring-gray-900/5   sm:rounded-full w-12 h-12 mx-2">12:07</div>
+                        <div className="relative bg-zinc-800  py-3 shadow-xl ring-1 ring-gray-900/5   sm:rounded-full w-12 h-12 mx-2">13:11</div>
+                        <div className="relative bg-zinc-800  py-3 shadow-xl ring-1 ring-gray-900/5   sm:rounded-full w-12 h-12 mx-2">18:52</div>
+
+
+                    </div>
+                    <div className="flex flex-col text-sm">
+                        <div>
+                            <span className="float-left p-3">Horas trabalhadas</span>
+                            <span className="float-right p-3">08:35</span>
+                        </div>
+                        <div>
+                            <span className="float-left p-3">Compensação</span>
+                            <span className="float-right p-3">00:51</span>
+                        </div>
+                        <div>
+                            <span className="float-left p-3">Hora extra 50%</span>
+                            <span className="float-right p-3">02:25</span>
+                        </div>
                     </div>
                 </div>
             </aside>
