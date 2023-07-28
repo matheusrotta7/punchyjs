00e3160e59ba66178768fe760c59604165a07f9b
Matheus Rotta <matheusrotta7@gmail.com>
2023-07-28 09:54:23 -0300

    change background color of datebox

--- src/app/components/DateBox.jsx
+++ src/app/components/DateBox.jsx
@@ -26,7 +26,7 @@ export default function DateBox (props) {
     }
 
     return (
-        <div className="relative bg-gray-300 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-12" onClick={onDateBoxClick}>
+        <div className="relative bg-zinc-400 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-12" onClick={onDateBoxClick}>
             <div className="absolute left-0 top-0 text-black/50 ml-1">{props.day}</div>
             <div className="absolute right-0 bottom-0 flex">
                 {renderPunchCircles()}
