5ba32dfb73b47e8ac5c417b99815c3d50c31fa1a
Matheus Rotta <matheusrotta7@gmail.com>
2023-08-31 16:37:09 -0300

    add translation to pricing screen and to main layout screen

--- src/app/main/layout.jsx
+++ src/app/main/layout.jsx
@@ -6,12 +6,25 @@ import LogoutButton from "../components/LogoutButton";
 
 import Image from 'next/image'
 import PunchyLogo from "../components/PunchyLogo";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { getDictionary } from "../dictionaries";
 
 
 export default function mainlayout({children}) {
+
+    const [dict, setDict] = useState()
+
+    const { locale } = useContext(AuthContext)
+
+    useEffect(() => {
+        setDict(getDictionary(locale))  
+    }, [])
+
     return (
         <>
-            <div className="h-screen flex flex-col">
+            {dict != undefined && dict != null ? 
+                <div className="h-screen flex flex-col">
                 <div className="flex flex-1">
                     <aside className="w-56 bg-zinc-950 border-r border-zinc-800 p-6">
                         <PunchyLogo/>
@@ -19,10 +32,10 @@ export default function mainlayout({children}) {
                             <LogIn /> <span className="ml-2">Login</span>
                         </Link>
                         <Link href="/main/landingscreen" className="flex p-2 hover:bg-zinc-800">
-                            <Monitor /> <span className="ml-2">Landing Page</span>
+                            <Monitor /> <span className="ml-2">{dict.mainlayoutscreen.aboutpunchy}</span>
                         </Link>
                         <Link href="/main/pricingscreen" className="flex p-2 hover:bg-zinc-800">
-                            <CircleDollarSign /> <span className="ml-2">Pricing</span>
+                            <CircleDollarSign /> <span className="ml-2">{dict.mainlayoutscreen.pricing}</span>
                         </Link>
                     </aside>
                     <main className="flex-1 p-6">
@@ -31,6 +44,10 @@ export default function mainlayout({children}) {
                 </div>
                 <footer className="bg-zinc-800 px-6 border-t border-zinc-700">Punchy: created by Matheus R.</footer>
             </div>
+            :
+            <></>
+            }
+            
         </>
     )
 }
--- src/app/main/pricingscreen/page.jsx
+++ src/app/main/pricingscreen/page.jsx
@@ -1,24 +1,38 @@
 'use client'
 
-import { useState } from "react"
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "@/app/contexts/AuthContext";
+import { getDictionary } from "@/app/dictionaries";
 
 export default function pricingscreen () {
 
     const [numOfEmployees, setNumberOfEmployees] = useState(3)
+    const [dict, setDict] = useState()
+
+    const { locale } = useContext(AuthContext)
+
+    useEffect(() => {
+        setDict(getDictionary(locale))  
+    }, [])
 
     return (
         <>
-            <div className="p-3">
+            {dict != null && dict != undefined ? 
+                <div className="p-3">
 
-                <h1>Our pricing is based on the number of employees in your company that will interact with Punchy</h1>
-                <h1>That includes three categories: employees, managers and one admin</h1>
-                <h1>Once a deal is closed, our tech support will enroll your company in our database and give you one admin access to your company</h1>
-                <h1>The admin will then be able to enroll managers and their employees, as long as the total number doesn't exceed the plan that was hired</h1>
-                <h2 className="mt-5">Test now how much it would cost per month by selecting the number of employees in your company that would interact with punchy (one admin, managers and employees):</h2>
-                <input type="range" min={3} max={1000} value={numOfEmployees} onChange={e => {setNumberOfEmployees(e.target.value)}} className="w-3/5"></input>
-                <h3>Number of employees: {numOfEmployees}</h3>
-                <h3>Estimated cost per month: R$ {numOfEmployees*5}</h3>
-            </div>
+                    <h1>{dict.pricingscreen.pricingsummary}</h1>
+                    <h1>{dict.pricingscreen.employeecategories}</h1>
+                    <h1>{dict.pricingscreen.accessdetails}</h1>
+                    <h1>{dict.pricingscreen.adminactions}</h1>
+                    <h2 className="mt-5">{dict.pricingscreen.testbudget}</h2>
+                    <input type="range" min={3} max={1000} value={numOfEmployees} onChange={e => {setNumberOfEmployees(e.target.value)}} className="w-3/5"></input>
+                    <h3>{dict.pricingscreen.numofemployees}: {numOfEmployees}</h3>
+                    <h3>{dict.pricingscreen.estimatedcost}: R$ {numOfEmployees*5}</h3>
+                </div>
+                :
+                <></>
+            }
+            
 
         </>
     )
