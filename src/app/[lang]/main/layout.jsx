1a9f3887ab44c46629748d8906734a79a8bc979c
Matheus Rotta <matheusrotta7@gmail.com>
2023-09-06 15:11:25 -0300

    changing locale path now working, still need to test dictionaries though

--- src/app/[lang]/main/layout.jsx
+++ src/app/[lang]/main/layout.jsx
@@ -4,16 +4,38 @@ import { Calendar, CircleDollarSign, Clock, Crown, LogIn, Monitor, User } from "
 import Link from "next/link";
 import LogoutButton from "../components/LogoutButton";
 import { AuthContext } from "@/app/contexts/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Image from 'next/image'
 import PunchyLogo from "../components/PunchyLogo";
+import ReactCountryFlag from "react-country-flag";
+import { usePathname, useRouter } from "next/navigation";
+
 
 
 export default function mainlayout({children}) {
 
-    const { locale } = useContext(AuthContext)
-    console.log("locale")
-    console.log(locale)
+    const { locale, setLocale } = useContext(AuthContext)
+
+    const [localeRef, setLocaleRef] = useState("")
+
+    const router = useRouter()
+    const pathname = usePathname()
+
+    useEffect(() => {
+        console.log("locale soaifjowiefjowifj")
+        console.log(locale)
+        setLocaleRef(locale)
+        console.log("pathname")
+        console.log(pathname)
+        var newpathname = pathname.replace(/^\/\w+\//i, "/" + locale + "/")
+        console.log("newpathname")
+        console.log(newpathname)
+        router.push(newpathname)
+    }, [locale])
+
+    function changeLocale(newLocale) {
+        setLocale(newLocale)
+    }
 
     return (
         <>
@@ -21,13 +43,37 @@ export default function mainlayout({children}) {
                 <div className="flex flex-1">
                     <aside className="w-56 bg-zinc-950 border-r border-zinc-800 p-6">
                         <PunchyLogo/>
-                        <Link href={locale + "/main/loginscreen"} className="flex p-2 hover:bg-zinc-800">
+                        <div className="p-2 flex ">
+                            <button onClick={() => changeLocale("pt")}>
+                                <ReactCountryFlag
+                                    
+                                    countryCode="BR" 
+                                    className="emojiFlag"
+                                    style={{
+                                        fontSize: '2em',
+                                        lineHeight: '2em',
+                                    }}
+                                /> <span className="text-lg mx-1"> / </span> 
+                            </button>
+                            <button onClick={() => changeLocale("en")}>
+                                <ReactCountryFlag
+                                    
+                                    countryCode="US" 
+                                    className="emojiFlag"
+                                    style={{
+                                        fontSize: '2em',
+                                        lineHeight: '2em',
+                                    }}
+                                />
+                            </button>
+                        </div>
+                        <Link href={"/" + localeRef + "/main/loginscreen"} className="flex p-2 hover:bg-zinc-800">
                             <LogIn /> <span className="ml-2">Login</span>
                         </Link>
-                        <Link href={locale + "/main/landingscreen"} className="flex p-2 hover:bg-zinc-800">
+                        <Link href={"/" + localeRef + "/main/landingscreen"} className="flex p-2 hover:bg-zinc-800">
                             <Monitor /> <span className="ml-2">Landing Page</span>
                         </Link>
-                        <Link href={locale + "/main/pricingscreen"} className="flex p-2 hover:bg-zinc-800">
+                        <Link href={"/" + localeRef + "/main/pricingscreen"} className="flex p-2 hover:bg-zinc-800">
                             <CircleDollarSign /> <span className="ml-2">Pricing</span>
                         </Link>
                     </aside>
@@ -40,3 +86,4 @@ export default function mainlayout({children}) {
         </>
     )
 }
+
